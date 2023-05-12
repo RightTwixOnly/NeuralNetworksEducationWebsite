28ac4bf54915f1df5c5f5c0ffab7b13266c99569
RightTwixOnly <98239902+RightTwixOnly@users.noreply.github.com>
2023-05-12 14:41:45 -0500

    mmm perceptron interactivity

--- perceptron.js
+++ perceptron.js
@@ -9,12 +9,13 @@ function Func(x){
 
 //creating the class perceptron
 class Perceptron {
-    constructor(n, learningRate = 0.1){
+    constructor(n, learningRate = 0.1){//n is the number of weights(4)
         this.weights = []; //weights (4)
         this.learningRate = learningRate;
         this.iterations = 0;
         this.results = [0, 0, 0, 0, 0, 0, 0]
         this.accuracy = 0;
+        this.bias = 0;
         for (let i = 0; i < n; i++) {//set the 4 weights to 0
             this.weights.push(0);
         }
@@ -31,6 +32,7 @@ class Perceptron {
         for (let i in this.weights){//for each weight (4)
             weightSum += this.weights[i] * x[i];//multiply each weight by forecast
         }
+        weightSum += this.bias
         // return the F-function of the sum of all the weights
         return (Func(weightSum));
     }
@@ -50,6 +52,7 @@ class Perceptron {
                     //weight balance for each weight in the day
                     for (let z = 0; z < ((this.weights).length); z++){
                         this.weights[z] = this.weights[z] + this.learningRate * (actual[k]-this.results[k]) * forecast[k][z]
+                        this.bias = this.bias + this.learningRate * (actual[k]-this.results[k])
                     }          
                 }  
             }
@@ -74,15 +77,25 @@ class Perceptron {
     }
 }
 
-forecast = [[75, 1, 0, 1], [98, 3, 0, 1], [70, 1, 0.3, 0], [60, 2, 7.0, -1], [75, 3, 3.0, -2], [80, 2, 0.3, 0], [80, 1, 0, 1]];
-test = [1, 0, 1, 0, 0, 1, 1];
+//forecast of a week of days, each day contains (in order), [temp, wind, mm rain, overcast]
+//wind: 1-3, 1: little to no wind, 2: moderate wind, 3: very windy
+//overcast: -2.0-1.0: -2: stormy, -1: rainy, 0: cloudy, 1: sunny
+forecast = [[75, 1, 0, 1], [98, 3, 0, 1], [70, 1, 0.3, 0], [60, 2, 7.0, -1], [75, 3, 3.0, -2], [80, 2, 0.3, 0], [80, 1, 0, 1], [72, 2, 0, 1], [44, 0, 0, 3], [59, 3, 6.3, -2], [52, 1, 3.2, -1], [55, 2, 1.5, -1], [60, 3, 0, 1], [69, 1, 0, 1]];
 
+real = [1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1];//answer key for if the day is nice or not
+
+//function  to create a perceptron
 function start(){
-    brain = new Perceptron(4);
-    brain.train(forecast, test, 2900);
-    console.log(brain.test());
-    console.log(brain.accuracyScore(test))
-    document.write(brain.accuracyScore(test))
+    meteorologist = new Perceptron(4);
+}
+
+function iterate(){
+    meteorologist.train(forecast, real, 250);
+    console.log(meteorologist.test());
+    console.log(meteorologist.accuracyScore(real))
+    document.getElementById('weights').innerHTML = "Weights after 250 iterations:" + meteorologist.weights
+    document.getElementById('accuracy').innerHTML = "Accuracy after 250 iterations:" + (meteorologist.accuracyScore(real))
+    document.getElementById('bias').innerHTML = "Bias after 250 iterations:" + (meteorologist.bias)
 }
 
 
